f95e4e72283fef16794d7446b91851a1bb792ce8
Sergei <mmylodd@gmail.com>
2020-05-31 12:55:56 +0300

    Feature: initial commit [ref: no-ref]

---
+++ src/main/java/ru/shop/clothes/ClothesApplication.java
@@ -0,0 +1,13 @@
+package ru.shop.clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesApplication.class, args);
+	}
+
+}
